7bcf13a4dfe040a98ac0777c9ba1140d7c4dc478
meliad2 <david.melia@ii.co.uk>
2022-11-30 12:19:37 +0000

    boot 3.0

--- src/main/java/uk/co/dave/consumer/fxrate/FxRateConsumerApplication.java
+++ src/main/java/uk/co/dave/consumer/fxrate/FxRateConsumerApplication.java
@@ -2,12 +2,10 @@ package uk.co.dave.consumer.fxrate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.schema.registry.client.EnableSchemaRegistryClient;
 
 
 
 @SpringBootApplication
-@EnableSchemaRegistryClient
 public class FxRateConsumerApplication {
   public static void main(String[] args) {
     SpringApplication.run(FxRateConsumerApplication.class, args);
